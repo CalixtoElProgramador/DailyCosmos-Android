243a3a83e7116d45ca3b4b5267b93c9cb3ec8f3a
CalixtoElProgramador <calixto.programador@gmail.com>
2021-12-17 19:09:37 -0600

    style: Screenshots added

--- app/src/main/java/com/listocalixto/dailycosmos/domain/apod/APODWebService.kt
+++ app/src/main/java/com/listocalixto/dailycosmos/domain/apod/APODWebService.kt
@@ -1,12 +1,11 @@
-    package com.listocalixto.dailycosmos.domain.apod
+package com.listocalixto.dailycosmos.domain.apod
 
 import com.google.gson.GsonBuilder
 import com.listocalixto.dailycosmos.application.AppConstants
 import com.listocalixto.dailycosmos.data.model.APOD
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface APODWebService {
 
